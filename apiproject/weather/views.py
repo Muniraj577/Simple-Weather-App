bccc6feff3446773da3d2bd1d47dd6c0970d69d1
Muniraj577 <munirajrajbanshi@gmail.com>
2020-02-25 01:45:35 +0545

    forms.py

--- apiproject/weather/views.py
+++ apiproject/weather/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests
 from .models import City
+from .forms import CityForm
 
 
 
@@ -8,8 +9,12 @@ from .models import City
 
 
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=YOUR API KEY'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=28916ea3edb082d74735a5cc7a627253'
     cities = City.objects.all()
+    if request.method == "POST":
+        form = CityForm(request.POST)
+        form.save()
+    form = CityForm()
     weather_data = []
     for city in cities:
         city_weather = requests.get(url.format(city)).json()
@@ -22,3 +27,4 @@ def index(request):
         weather_data.append(weather)
     context = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather/index.html', context)
+
